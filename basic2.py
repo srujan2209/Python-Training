675885109b3a73d30976564879ab17abe9069c1d
srujan <srujankumar1996@gmail.com>
2020-08-12 10:06:44 -0400

    specifies when our age is 100 years

---
+++ basic2.py
@@ -0,0 +1,8 @@
+# Create a program that asks the user to enter their name and their age.
+# Print out a message addressed to them that tells them the year that they will turn 100 years old.
+
+a=int(input("Enter age: "))
+
+year=int(2020-a)+100
+
+print("The year when you turn 100 years old is: ",year)
