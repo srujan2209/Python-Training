6a9a17b7a15da7783a6e12e677e88e6024e51dde
srujan <srujankumar1996@gmail.com>
2020-08-24 04:31:22 -0400

    Types of logging in python

---
+++ logg.py
@@ -0,0 +1,16 @@
+#  logging
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+print("hello world")
+
+logging.debug("Hello World")
+logging.info("It's info message")
+logging.warning("It's warning message")
+print("Hello Wor;d")
+logging.error("it's error message")
+logging.critical("It's critical message")
+
+
+print("Completed")
